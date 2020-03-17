c492458c726a4012ac4bf1ae1006547eb80319a8
DashL <blwkkz@gmail.com>
2020-03-17 21:50:01 +0300

    add count

--- lab4.py
+++ lab4.py
@@ -1,14 +1,26 @@
 import pymorphy2
 
-s = "Я люблю яблоко."
+s = "Я люблю красное красивое яблоко."
 lst = s.replace('.', '').split()
 print(lst)
 
 morph = pymorphy2.MorphAnalyzer()
 
+countADJ = 0
+countADVERB = 0
+countVERB = 0
+
 for i in lst:
     l = morph.parse(i)[0]
     print(l.tag.POS)
+    if l.tag.POS == ('ADJF' or 'ADJS'):
+        countADJ += 1  # подсчет прилагательных
+    elif l.tag.POS == 'ADVB':
+        countADVERB += 1  # подсчет наречий
+    elif l.tag.POS == ('VERB' or 'INFN'):
+        countVERB += 1  # подсчет глаголов
+
+print(countADJ)
 
 # Из исходников:
 # PARTS_OF_SPEECH = frozenset([
