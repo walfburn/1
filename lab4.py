c6880d23ff334765d910ef64784729c4cb535af4
DashL <blwkkz@gmail.com>
2020-03-30 23:40:04 +0300

    delete .txt, add def lab4

--- lab4.py
+++ lab4.py
@@ -1,31 +1,35 @@
 import pymorphy2
 import re
 
+data = 'Дай, Джим, на счастье лапу мне, Такую лапу не видал я сроду. Давай с тобой полаем при луне На тихую, бесшумную: погоду.'
 
-handle = open("text.txt", "r")
-data = handle.read()
-data = re.sub(r'[^\w\s]','', data)  # удаление пунктуации
-lst = data.split()  # текст разбит по словам
 
-morph = pymorphy2.MorphAnalyzer()
+def lab4(data):
+    data = re.sub(r'[^\w\s]', '', data)  # удаление пунктуации
+    lst = data.split()  # текст разбит по словам
 
-countADJ = 0
-countADVERB = 0
-countVERB = 0
+    morph = pymorphy2.MorphAnalyzer()
 
-for i in lst:
-    l = morph.parse(i)[0]
-    if l.tag.POS == ('ADJF' or 'ADJS'):
-        countADJ += 1  # подсчет прилагательных
-    elif l.tag.POS == 'ADVB':
-        countADVERB += 1  # подсчет наречий
-    elif l.tag.POS == ('VERB' or 'INFN'):
-        countVERB += 1  # подсчет глаголов
+    countADJ = 0
+    countADVERB = 0
+    countVERB = 0
 
-print('В тексте найдено: '
-      + str(countADJ) + ' прилагательных, '
-      + str(countADVERB) + ' наречий, '
-      + str(countVERB) + ' глаголов')
+    for i in lst:
+        l = morph.parse(i)[0]
+        if l.tag.POS == ('ADJF' or 'ADJS'):
+            countADJ += 1  # подсчет прилагательных
+        elif l.tag.POS == 'ADVB':
+            countADVERB += 1  # подсчет наречий
+        elif l.tag.POS == ('VERB' or 'INFN'):
+            countVERB += 1  # подсчет глаголов
 
-handle.close()
+    print('В тексте найдено: '
+          + str(countADJ) + ' прилагательных, '
+          + str(countADVERB) + ' наречий, '
+          + str(countVERB) + ' глаголов')
+
+    return countADJ, countADVERB, countVERB
+
+
+print(lab4(data))
 
